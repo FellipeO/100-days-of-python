55fb09569cfca5dc578ba3e9f739c122dd82b961
Fellipe <fellipeosilva2@gmail.com>
2026-06-04 10:08:22 -0300

    feat: concluido o dia 01

---
+++ Day 01 - Beginner - Working with Variables in Python to Manage Data/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Hello! Welcome to the Band Name Generator!")
+city = input("What city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Band Name: " + city + " " + pet)

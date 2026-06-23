db849119cbd9375f86b18a64b05b37bbf3118425
FellipeO <fellipeosilva2@gmail.com>
2026-06-23 00:38:04 -0300

    fix: adicionado tratamento de exceções ao código

--- Day 26 - Intermediate - List Comprehension and the NATO Alphabet/main.py
+++ Day 26 - Intermediate - List Comprehension and the NATO Alphabet/main.py
@@ -5,8 +5,14 @@ alpha_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 alpha_dict = {row.letter:row.code for (index,row) in alpha_df.iterrows()}
 print(alpha_dict)
 
-word = input("Type a word: ").upper()
-alpha_list = [alpha_dict[letter] for letter in word]
+def generate_phonetic():
+    word = input("Type a word: ").upper()
+    try:
+        alpha_list = [alpha_dict[letter] for letter in word]
+    except KeyError:
+        print("That's not a word.")
+        generate_phonetic()
+    else:
+        print(alpha_list)
 
-print(alpha_list)
-
+generate_phonetic()
